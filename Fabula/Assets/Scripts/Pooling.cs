25dbbe642ccdd78f6fcc7e7b62ad04f4299c85e5
Alper <alkilicalp@gmail.com>
2020-05-01 23:51:51 +0300

    First Commit

---
+++ Fabula/Assets/Scripts/Pooling.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pooling : MonoBehaviour
+{
+    public GameObject wall;
+    public GameObject movableCube;
+
+    public List<GameObject> wallPool;
+    public List<GameObject> movableCubePool;
+
+    public GameObject getWall(){
+        if(wallPool.Count == 0){
+            GameObject _tmpWall = Instantiate(wall);
+            return _tmpWall;
+        }
+        else{
+            GameObject _tmpWall = wallPool[0];
+            movableCubePool.RemoveAt(0);
+            _tmpWall.SetActive(true);
+            return _tmpWall;
+        }
+    }
+
+    public GameObject getMovableCube(){
+        if(movableCubePool.Count == 0){
+            GameObject _tmpWall = Instantiate(movableCube);
+            return _tmpWall;
+        }
+        else{
+            GameObject _tmpWall = movableCubePool[0];
+            movableCubePool.RemoveAt(0);
+            _tmpWall.SetActive(true);
+            return _tmpWall;
+        }
+    }
+}
